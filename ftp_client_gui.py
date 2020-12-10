000c2ce54c171ecdc35000b5c749a3c3c45f8418
JP7SRW <shou.3ky@gmail.com>
2020-12-10 14:26:23 +0900

    クライアント側のGUI試作ファイルを追加しました

---
+++ ftp_client_gui.py
@@ -0,0 +1,12 @@
+import tkinter
+from tkinter import ttk
+
+# メインウィンドウ
+main_win = tkinter.Tk()
+main_win.title("FTPサーバをに接続する")
+main_win.geometry("500x120")
+
+# メインフレーム
+main_frm = ttk.Frame(main_win)
+
+main_win.mainloop()
