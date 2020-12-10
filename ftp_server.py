d86eae73646099fb82d35c96a9d149b0ba1fdc1d
JP7SRW <shou.3ky@gmail.com>
2020-12-10 14:04:20 +0900

    コンフリクトを解消しました

--- ftp_server.py
+++ ftp_server.py
@@ -2,12 +2,6 @@
 import pyftpdlib.authorizers
 import pyftpdlib.handlers
 import pyftpdlib.servers
-<<<<<<< HEAD
-
-# 認証ユーザーを作る
-authorizer = pyftpdlib.authorizers.DummyAuthorizer()
-authorizer.add_user('user', 'password', 'C:/Users/shou/Desktop/programming/network_programming', perm='elradfmw')
-=======
 import sys
 
 # コマンドライン引数の判断
@@ -21,7 +15,6 @@ if argvlen < 1:
 # 認証ユーザーを作る
 authorizer = pyftpdlib.authorizers.DummyAuthorizer()
 authorizer.add_user('user', 'password', directory, perm='elradfmw')
->>>>>>> 750e13caa4d201a06e65f811101a1a7539370658
 
 # 個々の接続を管理するハンドラーを作る
 handler = pyftpdlib.handlers.FTPHandler
@@ -30,8 +23,3 @@ handler.authorizer = authorizer
 # FTPサーバーを立ち上げる
 server = pyftpdlib.servers.FTPServer(("127.0.0.1", 21), handler)
 server.serve_forever()
-<<<<<<< HEAD
-=======
-
-# hoge
->>>>>>> 750e13caa4d201a06e65f811101a1a7539370658
